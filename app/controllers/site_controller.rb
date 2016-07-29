883b437521785312cc3d767901e40bc071fbfcdd
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-07-28 21:22:51 -0400

    second bonus complete

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,11 +4,27 @@ class SiteController < ApplicationController
   end
 
   def create
+    # url in path
+    if !params[:words].nil?
+      @u = Url.new
+      @u.long_url = params[:words]
+      begin
+        @u.shortened_url = Randomstring.generate(4);
+      end while(!Url.find_by(shortened_url: @u.shortened_url).nil?)
+      if @u.save
+        flash[:notice] = "success"
+        render 'new'
+      else
+        render 'new'
+      end
 
-    if (!Url.find_by(long_url: site_params[:long_url]).nil?)
+    # url already in db
+    elsif (!Url.find_by(long_url: site_params[:long_url]).nil?)
       @u = Url.find_by(long_url: site_params[:long_url])
       flash[:notice] = "success"
       render 'new'
+
+    # create new url from form
     else
       @u = Url.new(site_params)
       begin
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   post '/' => 'site#create'
   patch '/' => 'site#create'
 
+  get '/:words', to: 'site#create', :constraints => {:words => /.*/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# URI.parse("http://localhost:3000/https://www.pandora.com"). Figure it out.
+#validate in controller eventually.
